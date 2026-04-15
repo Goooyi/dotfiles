fbf681ad1fda574dcc7ec002864735b7165a70d7
Goooyi <gaaoyi@gmail.com>
2026-04-15 16:38:34 +0800

    udpate

--- install.sh
+++ install.sh
@@ -7,6 +7,37 @@ STAMP="$(date +%Y%m%d%H%M%S)"
 DOTFILES_SKIP_ZSH="${DOTFILES_SKIP_ZSH:-0}"
 
 mkdir -p "${TARGET_HOME}/.config"
+mkdir -p "${TARGET_HOME}/.local/bin"
+
+install_starship() {
+  local bin_dir="$1"
+  local os="" arch="" target="" tmp_dir=""
+
+  case "$(uname -s)" in
+    Linux) os="unknown-linux-gnu" ;;
+    Darwin) os="apple-darwin" ;;
+    *)
+      echo "warning: unsupported OS for starship install: $(uname -s)" >&2
+      return 1
+      ;;
+  esac
+
+  case "$(uname -m)" in
+    x86_64|amd64) arch="x86_64" ;;
+    arm64|aarch64) arch="aarch64" ;;
+    *)
+      echo "warning: unsupported architecture for starship install: $(uname -m)" >&2
+      return 1
+      ;;
+  esac
+
+  target="${arch}-${os}"
+  tmp_dir="$(mktemp -d)"
+  curl -fsSL "https://github.com/starship/starship/releases/latest/download/starship-${target}.tar.gz" -o "${tmp_dir}/starship.tgz"
+  tar -xzf "${tmp_dir}/starship.tgz" -C "${tmp_dir}"
+  install -m 755 "${tmp_dir}/starship" "${bin_dir}/starship"
+  rm -rf "${tmp_dir}"
+}
 
 ensure_repo() {
   local url="$1"
@@ -50,6 +81,10 @@ if [[ "${DOTFILES_SKIP_ZSH}" != "1" ]]; then
   ensure_repo "https://github.com/zsh-users/zsh-syntax-highlighting.git" "${TARGET_HOME}/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting"
 fi
 ensure_repo "https://github.com/tpope/vim-sensible.git" "${TARGET_HOME}/.vim/pack/yigao/start/vim-sensible"
+
+if ! command -v starship >/dev/null 2>&1; then
+  install_starship "${TARGET_HOME}/.local/bin"
+fi
 
 if [[ "${DOTFILES_SKIP_ZSH}" != "1" ]]; then
   link_file "${REPO_ROOT}/zsh/.zshrc" "${TARGET_HOME}/.zshrc"
